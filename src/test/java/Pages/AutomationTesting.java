31d3830cb40d5c2634240047a45c6a848d14f3f0
Elif Atci <eelif0600@gmail.com>
2024-01-22 23:25:31 +0300

    22.01/Locators added

--- src/test/java/Pages/AutomationTesting.java
+++ src/test/java/Pages/AutomationTesting.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class AutomationTesting {
 
     public AutomationTesting(){
@@ -16,5 +18,18 @@ public class AutomationTesting {
     @FindBy(xpath = "(//*[@class='n2-ow'])[1]") public WebElement iconSliderRight;
     @FindBy(xpath = "(//*[@class='n2-ow'])[2]") public WebElement iconSliderLeft;
     @FindBy(className = "pull-down") public WebElement iconPullDown;
+    @FindBy(css = ".row_inner_wrapper") public List<WebElement> listArrivals;
+    @FindBy(xpath = "//*[@title='Selenium Ruby']") public WebElement imageFirstArrival;
+    @FindBy(css = ".single_add_to_cart_button.button.alt") public WebElement buttonAddToBasket;
+    @FindBy(className = "woocommerce-LoopProduct-link") public List<WebElement> listProducts;
+    @FindBy(css = ".product_title.entry-title") public WebElement textThinkinginHTML;
+    @FindBy(xpath = "//*[@title='Thinking in HTML']") public WebElement imageThinkingProduct;
+    @FindBy(css = ".button.wc-forward") public WebElement buttonViewBasket;
+    @FindBy(css = ".description_tab.active") public WebElement buttonDescription;
+    @FindBy(xpath = "(//*[@class='layoutArea'])[2]") public WebElement textProductDescription;
+
+
+
+
 
 }
